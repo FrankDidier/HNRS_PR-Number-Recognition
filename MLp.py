0477ffbaa0e8323e74d39b755422a93d97f7194a
ISINGIZWE DIDIER FRANK <ididierfrank@yahoo.fr>
2020-01-11 04:19:51 +0800

    Add files via upload

---
+++ MLp.py
@@ -0,0 +1,74 @@
+from PIL import Image
+import numpy as np
+
+import matplotlib.pyplot as plt
+import time
+
+def threshold(imageArray):
+
+    balanceAr = []
+    newAr = imageArray
+    from statistics import mean
+    for eachRow in imageArray:
+        for eachPix in eachRow:
+            avgNum = mean(eachPix[:3])
+            balanceAr.append(avgNum)
+
+    balance = mean(balanceAr)
+    for eachRow in newAr:
+        for eachPix in eachRow:
+            if mean(eachPix[:3]) > balance:
+                eachPix[0] = 255
+                eachPix[1] = 255
+                eachPix[2] = 255
+                eachPix[3] = 255
+            else:
+                eachPix[0] = 0
+                eachPix[1] = 0
+                eachPix[2] = 0
+                eachPix[3] = 255
+    return newAr
+
+def createExamples():
+    numberArrayExamples = open('numArEx.txt','a')
+    numbersWeHave = range(1,10)
+    for eachNum in numbersWeHave:
+        #print eachNum
+        for furtherNum in numbersWeHave:
+            # you could also literally add it *.1 and have it create
+            # an actual float, but, since in the end we are going
+            # to use it as a string, this way will work.
+            print(str(eachNum)+'.'+str(furtherNum))
+            imgFilePath = 'images/numbers/'+str(eachNum)+'.'+str(furtherNum)+'.png'
+            ei = Image.open(imgFilePath)
+            eiar = np.array(ei)
+            eiarl = str(eiar.tolist())
+
+            print(eiarl)
+            lineToWrite = str(eachNum)+'::'+eiarl+'\n'
+            numberArrayExamples.write(lineToWrite)
+
+
+i = Image.open('images/numbers/0.1.png')
+iar = np.array(i)
+i2 = Image.open('images/numbers/y0.4.png')
+iar2 = np.array(i2)
+i3 = Image.open('images/numbers/y0.5.png')
+iar3 = np.array(i3)
+i4 = Image.open('images/numbers/9.3.png')
+iar4 = np.array(i4)
+
+
+fig = plt.figure()
+ax1 = plt.subplot2grid((8,6),(0,0), rowspan=4, colspan=3)
+ax2 = plt.subplot2grid((8,6),(4,0), rowspan=4, colspan=3)
+ax3 = plt.subplot2grid((8,6),(0,3), rowspan=4, colspan=3)
+ax4 = plt.subplot2grid((8,6),(4,3), rowspan=4, colspan=3)
+
+ax1.imshow(iar)
+ax2.imshow(iar2)
+ax3.imshow(iar3)
+ax4.imshow(iar4)
+
+createExamples()
+plt.show()
